281498a964acf23813293ce0fc9debc7770a72d1
Seanlinsanity <seanlin1016@gmail.com>
2020-04-02 19:18:42 +0800

    Initial Commit

---
+++ AppStoreJSONAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONAPI
+//
+//  Created by Sean on 2020/4/2.
+//  Copyright © 2020 Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
